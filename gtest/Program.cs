d103a67c42bb74fdc3a4f90b5ddc1d7d75446903
VladPulyak <vlad.pulyak03@mail.ru>
2022-07-15 13:58:17 +0300

    Added branch height

--- gtest/Program.cs
+++ gtest/Program.cs
@@ -9,12 +9,14 @@ namespace testGit
             string name = string.Empty;
             string surname = string.Empty;
             int age = 0;
+            int height = 0;
             int responce = 0;
             while (responce != -1)
             {
                 Console.WriteLine("Enter 0 to enter your name" + Environment.NewLine +
-                                    "Enter 1 to enter your surname" + Environment.NewLine +
+                                    "Enter 1 to enter your surname" + Environment.NewLine +                                    
                                     "Enter 2 to enter your age" + Environment.NewLine +
+                                    "Enter 3 to enter your height" + Environment.NewLine +
                                     "Enter -2 to print resume" + Environment.NewLine +
                                     "Enter -1 to EXIT");
                 responce = int.Parse(Console.ReadLine());
@@ -38,11 +40,18 @@ namespace testGit
                             age = int.Parse(Console.ReadLine());
                             break;
                         }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter your height");
+                            height = int.Parse(Console.ReadLine());
+                            break;
+                        }
                     case -2:
                         {
                             Console.WriteLine(name);
                             Console.WriteLine(surname);
                             Console.WriteLine(age);
+                            Console.WriteLine(height);
                             break;
                         }
                     case -1:
